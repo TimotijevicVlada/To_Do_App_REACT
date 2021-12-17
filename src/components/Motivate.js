eaa4446f7155c2c93c318f840b8d7f5de3e8c0a0
Vladimir <timotijevicvlada@gmail.com>
2021-12-17 20:07:17 +0100

    Fetching motivate msg and display on the screen.

--- src/components/Motivate.js
+++ src/components/Motivate.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Motivate = () => {
+
+    const [motivateMsg, setMotivateMsg] = useState([]);
+
+    //Function that get motivational message from API
+    const getMotivate = async () => {
+        const response = await axios.get("https://run.mocky.io/v3/dee319cd-aa8b-4e30-b86e-3743237fca55");
+        console.log(response.data.motivational_quotes);
+        setMotivateMsg(response.data.motivational_quotes);
+    }
+
+    useEffect(() => {
+        getMotivate();
+    }, []);
 
 
     return (
@@ -9,7 +23,17 @@ const Motivate = () => {
             <div className='motivate_header'>
                 <Link className='link' to="/">I am motivated!</Link>
             </div>
-            <h2>Hello from motivate</h2>
+            <div className='title'>
+                <h2>Motivational messages</h2>
+            </div>
+            <div className='motivate_msg'>
+                {motivateMsg.map((item, index) => (
+                    <div className='msg' key={index}>
+                        <div className='quote'>"{item.quote}"</div>
+                        <div className='author'>-{item.author}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
